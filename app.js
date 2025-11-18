fe63a588e4441d352d0858d5b9756b59509feb10
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-18 16:48:13 +0500

    Add middleware to parse form data to app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("node:path");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
