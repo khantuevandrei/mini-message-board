bfce56880f5b2d9b28fbed79cd6320b391610881
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-18 17:14:00 +0500

    Add a route for individual message page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,4 +7,14 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+indexRouter.get("/:indexId", (req, res) => {
+  const { indexId } = req.params;
+  res.render("message", {
+    id: indexId,
+    text: messages[indexId].text,
+    user: messages[indexId].user,
+    added: messages[indexId].added,
+  });
+});
+
 module.exports = indexRouter;
